c2ff098bbe92b51435092ed7fa10fdf7fd31371b
Dan McEwan <dan.mcewan@gmail.com>
2013-05-11 02:51:37 -0400

    stripping css id for form from js target (missing in rubik)

--- js/admin.js
+++ js/admin.js
@@ -2,14 +2,14 @@
   Drupal.behaviors.followMe = {
     attach: function (context, settings) {
 
-      $('#edit-follow-me-type .form-type-textfield').each(function() {
+      $('.form-type-textfield').each(function() {
         if ( $(this).prev().children('input').is(':checked') == 0 ) {
           $(this).hide();
         }
 
       });
 
-      $('#edit-follow-me-type .form-type-checkbox').click(function(){
+      $('.form-type-checkbox').click(function(){
         $(this).next().toggle('fast');
 
       });
